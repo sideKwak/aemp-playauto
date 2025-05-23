b9a16e6e496f589f55054faea52e5792788e9c5b
blackDesertShinBeolUi <yanghye0321@naver.com>
2025-05-23 18:30:55 +0900

    로그 필터 추가

--- backend/nest-app/src/common/filters/http-exception.filter.ts
+++ backend/nest-app/src/common/filters/http-exception.filter.ts
@@ -1,35 +1,41 @@
 import {
-    ExceptionFilter,
-    Catch,
-    ArgumentsHost,
-    HttpException,
-    HttpStatus,
-  } from '@nestjs/common';
-  import { Request, Response } from 'express';
-  
-  @Catch() // 모든 예외 캐치
-  export class AllExceptionsFilter implements ExceptionFilter {
-    catch(exception: unknown, host: ArgumentsHost) {
-      const ctx = host.switchToHttp();
-      const response = ctx.getResponse<Response>();
-      const request = ctx.getRequest<Request>();
-  
-      const status =
-        exception instanceof HttpException
-          ? exception.getStatus()
-          : HttpStatus.INTERNAL_SERVER_ERROR;
-  
-      const message =
-        exception instanceof HttpException
-          ? exception.getResponse()
-          : 'Internal server error';
-  
-      response.status(status).json({
-        success: false,
-        statusCode: status, 
-        timestamp: new Date().toISOString(),
-        path: request.url,
-        message,
-      });
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  private readonly logger = new Logger(AllExceptionsFilter.name);
+
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    let status = 500;
+    let message = 'Internal server error';
+
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      message = exception.message;
     }
+
+    this.logger.error(
+      `❌ ${request.method} ${request.url} - ${status} - ${message}`,
+      exception instanceof Error ? exception.stack : '',
+    );
+
+    response.status(status).json({
+      success: false,
+      statusCode: status,
+      message,
+      path: request.url,
+      timestamp: new Date().toISOString(),
+    });
   }
+}
